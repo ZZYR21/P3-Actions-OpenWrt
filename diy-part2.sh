fc027dd05899d7455f1ba38fea0304160227a041
ZZYR21 <48266983+ZZYR21@users.noreply.github.com>
2020-06-28 14:16:58 +0800

    ip

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,4 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/95.86.21.21/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/95.86.21.21/g' package/base-files/files/bin/config_generate
